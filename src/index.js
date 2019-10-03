f6eb76455a9a9a1092755375b4a620c23db7aed7
David Bee <baeddavid@gmail.com>
2019-10-03 11:26:08 -0500

    scaffold index file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 const { GraphQLServer } = require('graphql-yoga');
+const { prisma } = require('./generated/prisma-client');
+
+const Resolvers = {
+
+};
+
+const server = new GraphQLServer({
+    typeDefs: './src/schema.graphql',
+    resolvers,
+    context: request => {
+        return {
+            ...request,
+            prisma,
+        }
+    },
+});
+
+server.start(() => console.log(`Server is running on http://localhost:4000`));
