ce5c69f9515830cacaa4ca93536cbb2d550fe994
David Bee <baeddavid@gmail.com>
2019-10-04 00:55:52 -0500

    add get closest query within +- 1 lng/lat of current location

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -24,7 +24,31 @@ async function feed(parent, args, context, info) {
         count,
     };
 }
+async function getClosest(parent, args, context, info) {
+    const where = {
+        AND: [{
+            AND: [
+                { lat_lt: args.currentLat + 1 },
+                { lng_lt: args.currentLng + 1},
+            ]
+        }, {
+            AND: [
+                { lat_gt: args.currentLat - 1 },
+                { lng_gt: args.currentLng - 1},
+            ]
+        }]
+    };
+    console.log(where)
+    const bathrooms = await context.prisma.bathrooms({
+        where,
+    });
+
+    return {
+        bathrooms,
+    };
+}
 
 module.exports = {
     feed,
+    getClosest,
 }
